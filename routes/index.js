16785739f9f573e6c1e6c286955ce7e074b4c438
Andresr35 <andresjruiz@cpp.edu>
2023-10-08 16:09:44 -0700

    added ejs to display messages

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+
+// Sample Messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+module.exports = router;
