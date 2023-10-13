0bcf43e1d6860ee95d5fb7fb069facfbf26a79da
Andresr35 <andresjruiz@cpp.edu>
2023-10-12 21:48:40 -0700

    add in database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const Message = require("../models/message");
 
 // Sample Messages
 const messages = [
@@ -16,8 +17,24 @@ const messages = [
 ];
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
+  // console.log(await Message.countDocuments({}).exec());
   res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+router.post("/new", async function (req, res, next) {
+  const newMessage = new Message({
+    text: req.body.authorName,
+    user: req.body.message,
+    added: new Date(),
+  });
+  await newMessage.save();
+  messages.push(newMessage);
+  res.redirect("./");
 });
 
 module.exports = router;
